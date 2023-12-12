a630fd0eb4d9973b86fca6c93ced138ac2e0adc3
Arianno4ka <152208156+Arianno4ka@users.noreply.github.com>
2023-12-12 21:54:48 +0300

    Add files via upload

--- note.h
+++ note.h
@@ -1,5 +1,37 @@
 #pragma once
+#include <iostream>
+using namespace std;
 class note
 {
+private:
+    string firstName;
+    string lastName;
+    string phoneNumber;
+    int birthDate[3];
+
+public:
+    // Конструкторы
+    note();
+    note(const string& first, const string& last, const string& phone, const int* birth);
+    note(const note& other); // Конструктор копирования
+
+    // Деструктор
+    ~note();
+
+    // Методы доступа (getters и setters)
+    const string& getFirstName() const;
+    const string& getLastName() const;
+    const string& getPhoneNumber() const;
+    const int* getBirthDate() const;
+
+    void setFirstName(const string& first);
+    void setLastName(const string& last);
+    void setPhoneNumber(const string& phone);
+    void setBirthDate(const int* birth);
+
+    // Перегруженные операции вставки и извлечения
+    friend ostream& operator<<(std::ostream& os, const note& note);
+    friend istream& operator>>(std::istream& is, note& note);
 };
+
 
